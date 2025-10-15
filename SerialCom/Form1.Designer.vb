29d85e7897a0d7bec2e574837cdcbaddf0a6eedb
sahikjaman <79184185+sahikjaman@users.noreply.github.com>
2025-10-15 10:49:26 +0700

    Initial commit: Menambahkan semua file proyek awal

---
+++ SerialCom/Form1.Designer.vb
@@ -0,0 +1,437 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.dataGantry = New System.Windows.Forms.Label()
+        Me.datatrolley = New System.Windows.Forms.Label()
+        Me.datahoist = New System.Windows.Forms.Label()
+        Me.dataload = New System.Windows.Forms.Label()
+        Me.datalock = New System.Windows.Forms.Label()
+        Me.trolleyposition = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.dataalightmnet = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Button1.Location = New System.Drawing.Point(267, 45)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(159, 77)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Connect"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(28, 63)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(180, 37)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.RichTextBox2.Location = New System.Drawing.Point(15, 86)
+        Me.RichTextBox2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(432, 36)
+        Me.RichTextBox2.TabIndex = 5
+        Me.RichTextBox2.Text = ""
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label2.Location = New System.Drawing.Point(9, 43)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 29)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Input"
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Button4.Location = New System.Drawing.Point(267, 131)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(159, 77)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "Disconnect"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM6"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.ComboBox2.Items.AddRange(New Object() {"9600", "19200", "38400", "57600", "115200"})
+        Me.ComboBox2.Location = New System.Drawing.Point(28, 149)
+        Me.ComboBox2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(180, 37)
+        Me.ComboBox2.TabIndex = 9
+        Me.ComboBox2.Text = "9600"
+        Me.ComboBox2.UseWaitCursor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(81, 50)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 20)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Label3"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label12)
+        Me.GroupBox1.Controls.Add(Me.Label11)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.ComboBox2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 190)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.GroupBox1.Size = New System.Drawing.Size(458, 242)
+        Me.GroupBox1.TabIndex = 11
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Connection Method"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(28, 125)
+        Me.Label12.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(83, 20)
+        Me.Label12.TabIndex = 11
+        Me.Label12.Text = "Baudrate :"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(28, 34)
+        Me.Label11.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(79, 20)
+        Me.Label11.TabIndex = 10
+        Me.Label11.Text = "ComPort :"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.RichTextBox2)
+        Me.GroupBox3.Controls.Add(Me.Label2)
+        Me.GroupBox3.Location = New System.Drawing.Point(13, 20)
+        Me.GroupBox3.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Padding = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.GroupBox3.Size = New System.Drawing.Size(466, 145)
+        Me.GroupBox3.TabIndex = 13
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "GroupBox3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(35, 161)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(90, 20)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "Gantry      ="
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(35, 204)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(87, 20)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Trolley      ="
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(35, 244)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(87, 20)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "Hoist        ="
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Arial", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(32, 22)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(184, 37)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "Load        ="
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(281, 336)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(178, 20)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "kondisi lock-unlock       ="
+        '
+        'dataGantry
+        '
+        Me.dataGantry.AutoSize = True
+        Me.dataGantry.Location = New System.Drawing.Point(165, 161)
+        Me.dataGantry.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.dataGantry.Name = "dataGantry"
+        Me.dataGantry.Size = New System.Drawing.Size(66, 20)
+        Me.dataGantry.TabIndex = 20
+        Me.dataGantry.Text = "Label10"
+        '
+        'datatrolley
+        '
+        Me.datatrolley.AutoSize = True
+        Me.datatrolley.Location = New System.Drawing.Point(165, 204)
+        Me.datatrolley.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.datatrolley.Name = "datatrolley"
+        Me.datatrolley.Size = New System.Drawing.Size(66, 20)
+        Me.datatrolley.TabIndex = 21
+        Me.datatrolley.Text = "Label11"
+        '
+        'datahoist
+        '
+        Me.datahoist.AutoSize = True
+        Me.datahoist.Location = New System.Drawing.Point(165, 244)
+        Me.datahoist.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.datahoist.Name = "datahoist"
+        Me.datahoist.Size = New System.Drawing.Size(66, 20)
+        Me.datahoist.TabIndex = 22
+        Me.datahoist.Text = "Label12"
+        '
+        'dataload
+        '
+        Me.dataload.AutoSize = True
+        Me.dataload.Font = New System.Drawing.Font("Arial", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dataload.Location = New System.Drawing.Point(240, 22)
+        Me.dataload.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.dataload.Name = "dataload"
+        Me.dataload.Size = New System.Drawing.Size(138, 37)
+        Me.dataload.TabIndex = 23
+        Me.dataload.Text = "Label13"
+        '
+        'datalock
+        '
+        Me.datalock.AutoSize = True
+        Me.datalock.Location = New System.Drawing.Point(498, 336)
+        Me.datalock.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.datalock.Name = "datalock"
+        Me.datalock.Size = New System.Drawing.Size(66, 20)
+        Me.datalock.TabIndex = 24
+        Me.datalock.Text = "Label14"
+        '
+        'trolleyposition
+        '
+        Me.trolleyposition.AutoSize = True
+        Me.trolleyposition.Location = New System.Drawing.Point(333, 190)
+        Me.trolleyposition.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.trolleyposition.Name = "trolleyposition"
+        Me.trolleyposition.Size = New System.Drawing.Size(66, 20)
+        Me.trolleyposition.TabIndex = 26
+        Me.trolleyposition.Text = "Label16"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(363, 374)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(91, 20)
+        Me.Label10.TabIndex = 27
+        Me.Label10.Text = "alighment ="
+        '
+        'dataalightmnet
+        '
+        Me.dataalightmnet.AutoSize = True
+        Me.dataalightmnet.Location = New System.Drawing.Point(498, 374)
+        Me.dataalightmnet.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.dataalightmnet.Name = "dataalightmnet"
+        Me.dataalightmnet.Size = New System.Drawing.Size(66, 20)
+        Me.dataalightmnet.TabIndex = 28
+        Me.dataalightmnet.Text = "Label11"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label14)
+        Me.GroupBox2.Controls.Add(Me.Label13)
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.dataGantry)
+        Me.GroupBox2.Controls.Add(Me.dataalightmnet)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label10)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.trolleyposition)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.datalock)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.dataload)
+        Me.GroupBox2.Controls.Add(Me.Label8)
+        Me.GroupBox2.Controls.Add(Me.datahoist)
+        Me.GroupBox2.Controls.Add(Me.datatrolley)
+        Me.GroupBox2.Location = New System.Drawing.Point(504, 20)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(636, 452)
+        Me.GroupBox2.TabIndex = 29
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "GroupBox2"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(255, 161)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(33, 20)
+        Me.Label1.TabIndex = 29
+        Me.Label1.Text = "CM"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Arial", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(386, 22)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(82, 37)
+        Me.Label9.TabIndex = 30
+        Me.Label9.Text = "TON"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(255, 204)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(33, 20)
+        Me.Label13.TabIndex = 31
+        Me.Label13.Text = "CM"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(255, 250)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(33, 20)
+        Me.Label14.TabIndex = 32
+        Me.Label14.Text = "CM"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.ClientSize = New System.Drawing.Size(1208, 603)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents RichTextBox2 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents dataGantry As Label
+    Friend WithEvents datatrolley As Label
+    Friend WithEvents datahoist As Label
+    Friend WithEvents dataload As Label
+    Friend WithEvents datalock As Label
+    Friend WithEvents trolleyposition As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents dataalightmnet As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label13 As Label
+End Class
---
+++ SerialCom/Form1.vb
@@ -0,0 +1,114 @@
+﻿Imports System
+Imports System.Threading
+Imports System.IO.Ports
+Imports System.ComponentModel
+
+
+Public Class Form1
+    '------------------------------------------------
+    Dim myPort As Array
+    Delegate Sub SetTextCallback(ByVal [text] As String) 'Added to prevent threading errors during receiveing of data
+    Dim str As String
+    Dim arr As Array
+    Dim trolleypos As Int32
+    Dim datlock As Int32
+    Dim alightmnt As Int32
+    Dim timbang As Single
+
+
+
+    Private Sub Form1_Close()
+        SerialPort1.Close()
+    End Sub
+
+    '------------------------------------------------
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+        myPort = IO.Ports.SerialPort.GetPortNames()
+        ComboBox1.Items.AddRange(myPort)
+
+        'Button2.Enabled = False
+
+    End Sub
+
+
+    '------------------------------------------------
+    Private Sub ComboBox1_Click(sender As System.Object, e As System.EventArgs) Handles ComboBox1.Click
+    End Sub
+    '------------------------------------------------
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        SerialPort1.PortName = ComboBox1.Text
+        SerialPort1.BaudRate = ComboBox2.Text
+        'SerialPort1.Parity = IO.Ports.Parity.Even
+        'SerialPort1.DataBits = IO.Ports.SerialData.Chars
+        'SerialPort1.StopBits = IO.Ports.StopBits.One
+        SerialPort1.Open()
+        Button1.Enabled = False
+        'Button2.Enabled = True
+        Button4.Enabled = True
+
+    End Sub
+    '------------------------------------------------
+
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        SerialPort1.Close()
+        Button1.Enabled = True
+        ' Button2.Enabled = False
+        Button4.Enabled = False
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(sender As System.Object, e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        ReceivedText(SerialPort1.ReadExisting())
+    End Sub
+
+    Private Sub ReceivedText(ByVal [text] As String) 'input from ReadExisting
+        If Me.RichTextBox2.InvokeRequired Then
+            Dim x As New SetTextCallback(AddressOf ReceivedText)
+            Me.Invoke(x, New Object() {(text)})
+
+        Else
+            Me.RichTextBox2.Text = [text] 'append text
+            Label3.Text = text
+
+            str = text
+            timbang = (Convert.ToInt32(str.Substring(2, 2) + str.Substring(0, 2), 16)) / 10
+            dataload.Text = timbang
+            datahoist.Text = Convert.ToInt32(str.Substring(6, 2) + str.Substring(4, 2), 16)
+            dataGantry.Text = (Convert.ToInt32(str.Substring(14, 2) + str.Substring(12, 2) + str.Substring(10, 2) + str.Substring(8, 2), 16)) / 10
+            trolleypos = Convert.ToInt32(str.Substring(18, 2) + str.Substring(16, 2), 16)
+            datatrolley.Text = trolleypos
+            If trolleypos < 500 Then
+                trolleyposition.Text = "area parking"
+            Else
+                trolleyposition.Text = "area CY"
+
+            End If
+            datlock = Convert.ToInt32(str.Substring(22, 2) + str.Substring(20, 2), 16)
+            If datlock = 1 Then
+                datalock.Text = "lock"
+            Else
+                datalock.Text = "Unlock"
+            End If
+
+            'datachc
+            '    dataunlock.Text = Convert.ToInt32(str.Substring(46, 2) + str.Substring(44, 2) + str.Substring(42, 2) + str.Substring(40, 2), 16)
+
+            alightmnt = Convert.ToInt32(str.Substring(26, 2) + str.Substring(24, 2), 16)
+            If alightmnt = 1 Then
+                dataalightmnet.Text = "ON"
+            Else
+                dataalightmnet.Text = "OFF"
+            End If
+
+        End If
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
